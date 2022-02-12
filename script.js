95b14eba9ccc3827b86d6edf9d150ab5cc328527
thaqifazfar15 <thaqifazfar15@gmail.com>
2022-02-12 13:14:08 +0800

    Add logic and simple ui in browser

--- script.js
+++ script.js
@@ -1,75 +1,120 @@
-
-
-function computerPlay() { //a function to make the computer choose between rock, paper and scissor
-    const choice = ["ROCK", "PAPER", "SCISSOR"];
-    let num = Math.floor(Math.random()*choice.length);
-    return choice[num];
+function computerPlay() {
+  //a function to make the computer choose between rock, paper and scissor
+  const choice = ["rock", "paper", "scissor"];
+  let num = Math.floor(Math.random() * choice.length);
+  return choice[num];
 }
 
-
-function playerPlay() { //a function to get player choice
-    let playerPick = prompt("Enter your choice (Rock, Paper, Scissor): ");
-    let choice = playerPick.toUpperCase();
-    if (choice === "ROCK" || choice === "PAPER" || choice === "SCISSOR") { //to check whether it is the correct choice
-        return choice;
-    } else {
-        return "Game Error, Please Enter Correct Choice"
-        }
-}
-
-let rock = "ROCK"
-let paper = "PAPER"
-let scissor = "SCISSOR"
+let rock = "rock";
+let paper = "paper";
+let scissor = "scissor";
 
 function playRound(computerSelection, playerSelection) {
-    if (computerSelection === playerSelection) {
-        alert("Draw");
-        return "draw";
-    } else if (computerSelection === rock && playerSelection === paper) {
-        alert("You Win! Paper beats Rock");
-        return "win";
-    } else if (computerSelection === paper && playerSelection === rock) {
-        alert("You Lose! Paper beats Rock");
-        return "lose";
-    } else if (computerSelection === scissor && playerSelection === rock) {
-        alert("You Win! Rock beats Scissor");
-        return "win";
-    } else if (computerSelection === rock && playerSelection === scissor) {
-        alert("You Lose! Rock beats Scissor");
-        return "lose";
-    } else if (computerSelection === paper && playerSelection === scissor) {
-        alert("You Win! Scissor beats Rock");
-        return "win";
-    } else if (computerSelection === scissor && playerSelection === paper) {
-        alert("You Lose! Scissor beats Rock");
-        return "lose";
-    }
+  //to decide who wins
+  if (computerSelection === playerSelection) {
+    return "draw";
+  } else if (computerSelection === rock && playerSelection === paper) {
+    return "win";
+  } else if (computerSelection === paper && playerSelection === rock) {
+    return "lose";
+  } else if (computerSelection === scissor && playerSelection === rock) {
+    return "win";
+  } else if (computerSelection === rock && playerSelection === scissor) {
+    return "lose";
+  } else if (computerSelection === paper && playerSelection === scissor) {
+    return "win";
+  } else if (computerSelection === scissor && playerSelection === paper) {
+    return "lose";
+  }
+}
+
+let compScore = 0; //global variable
+let playerScore = 0; //global varibale
+
+let humanScore = document.getElementById("human-score");
+let computerScore = document.getElementById("computer-score");
+
+let winnerIs = document.getElementById("isWinner");
+
+let displayWinner = document.getElementById("display-winner");
+let btn = document.createElement("button");
+
+function reset() {
+  //reset the game so it can be played again
+  compScore = 0;
+  computerScore.innerHTML = compScore.toString();
+  playerScore = 0;
+  humanScore.innerHTML = playerScore.toString();
+  winnerIs.innerHTML = "";
+  displayWinner.removeChild(btn);
+}
+
+function alertWinner(compScore, playerScore) {
+  if ((compScore === playerScore) === 5) {
+    winnerIs.innerHTML = "It's a Draw!";
+    createRetryBtn();
+  } else if (compScore === 5) {
+    winnerIs.innerHTML = "You Lose!";
+    createRetryBtn();
+  } else if (playerScore === 5) {
+    winnerIs.innerHTML = "You Win!";
+    createRetryBtn();
+  }
+}
+function createRetryBtn() {
+  //for player to retry again
+  btn.classList.add("retry-again");
+  btn.innerHTML = "Play again?";
+  btn.addEventListener("click", function () {
+    reset();
+  });
+  displayWinner.appendChild(btn);
 }
 
 function game() {
+  //play game
 
-    for (i = 0; i < 5; i++) {
-        compScore = 0
-        playerScore = 0
+  let computerSelection = computerPlay(); //store computer choice into a variable
+  let winner = playRound(computerSelection, playerSelection);
 
-        let computerSelection = computerPlay(); //store computer choice into a variable
-        let playerSelection = playerPlay(); //store player choice into a variable
+  document.getElementById("computer-choice").innerHTML = computerSelection; //display computer choice
 
-        winner = playRound(computerSelection, playerSelection);
-
-        if (winner === "draw"){
-            playerScore += 0;
-        } else if (winner === "win") {
-            playerScore++;
-        } else {
-            compScore++;
-        }
+  if (winner === "draw") {
+  } else if (winner === "lose") {
+    compScore++;
+    computerScore.innerHTML = compScore.toString(); //update score
+    if (compScore === 5 || playerScore === 5) {
+      alertWinner(compScore, playerScore);
     }
-    if (compScore == playerScore) {
-        alert("Draw!")
-    } else if (compScore > playerScore) {
-        alert("Computer Win!")
-    } else {
-        alert("You Win!")
+  } else if (winner === "win") {
+    playerScore++;
+    humanScore.innerHTML = playerScore.toString(); //update score
+    if (compScore === 5 || playerScore === 5) {
+      alertWinner(compScore, playerScore);
     }
+  }
 }
+
+const chooseRock = document.querySelector(".rock");
+chooseRock.addEventListener("click", () => {
+  playerSelection = rock;
+  if (compScore < 5 && playerScore < 5) {
+    game();
+  }
+});
+
+const choosePaper = document.querySelector(".paper");
+choosePaper.addEventListener("click", () => {
+  playerSelection = paper;
+  if (compScore < 5 && playerScore < 5) {
+    game();
+  }
+});
+
+const chooseScissor = document.querySelector(".scissor");
+chooseScissor.addEventListener("click", () => {
+  playerSelection = scissor;
+  if (compScore < 5 && playerScore < 5) {
+    game();
+  }
+});
