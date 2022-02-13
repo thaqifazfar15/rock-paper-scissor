b9f068c4716ebdecb99dd62899cd5b51245165c8
thaqifazfar15 <thaqifazfar15@gmail.com>
2022-02-13 14:18:07 +0800

    Add responsive button

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-function computerPlay() {
-  //a function to make the computer choose between rock, paper and scissor
-  const choice = ["rock", "paper", "scissor"];
-  let num = Math.floor(Math.random() * choice.length);
-  return choice[num];
-}
-
 let rock = "rock";
 let paper = "paper";
 let scissor = "scissor";
-
-function playRound(computerSelection, playerSelection) {
-  //to decide who wins
-  if (computerSelection === playerSelection) {
-    return "draw";
-  } else if (computerSelection === rock && playerSelection === paper) {
-    return "win";
-  } else if (computerSelection === paper && playerSelection === rock) {
-    return "lose";
-  } else if (computerSelection === scissor && playerSelection === rock) {
-    return "win";
-  } else if (computerSelection === rock && playerSelection === scissor) {
-    return "lose";
-  } else if (computerSelection === paper && playerSelection === scissor) {
-    return "win";
-  } else if (computerSelection === scissor && playerSelection === paper) {
-    return "lose";
-  }
-}
 
 let compScore = 0; //global variable
 let playerScore = 0; //global varibale
@@ -39,6 +13,41 @@ let winnerIs = document.getElementById("isWinner");
 let displayWinner = document.getElementById("display-winner");
 let btn = document.createElement("button");
 
+let displayOutcome = document.getElementById("the-outcome");
+
+function computerPlay() {
+  //a function to make the computer choose between rock, paper and scissor
+  const choice = ["rock", "paper", "scissor"];
+  let num = Math.floor(Math.random() * choice.length);
+  return choice[num];
+}
+
+function playRound(computerSelection, playerSelection) {
+  //to decide who wins
+  if (computerSelection === playerSelection) {
+    displayOutcome.innerHTML = "It's a tie!";
+    return "draw";
+  } else if (computerSelection === rock && playerSelection === paper) {
+    displayOutcome.innerHTML = "You win, paper beat rock!";
+    return "win";
+  } else if (computerSelection === paper && playerSelection === rock) {
+    displayOutcome.innerHTML = "You lose, paper beat rock!";
+    return "lose";
+  } else if (computerSelection === scissor && playerSelection === rock) {
+    displayOutcome.innerHTML = "You win, rock beat scissor!";
+    return "win";
+  } else if (computerSelection === rock && playerSelection === scissor) {
+    displayOutcome.innerHTML = "You lose, rock beat scissor!";
+    return "lose";
+  } else if (computerSelection === paper && playerSelection === scissor) {
+    displayOutcome.innerHTML = "You win, scissor beat paper!";
+    return "win";
+  } else if (computerSelection === scissor && playerSelection === paper) {
+    displayOutcome.innerHTML = "You lose, scissor beat paper!";
+    return "lose";
+  }
+}
+
 function reset() {
   //reset the game so it can be played again
   compScore = 0;
@@ -47,6 +56,7 @@ function reset() {
   humanScore.innerHTML = playerScore.toString();
   winnerIs.innerHTML = "";
   displayWinner.removeChild(btn);
+  displayOutcome.innerHTML = "";
 }
 
 function alertWinner(compScore, playerScore) {
@@ -97,6 +107,10 @@ function game() {
 
 const chooseRock = document.querySelector(".rock");
 chooseRock.addEventListener("click", () => {
+  chooseRock.classList.add("clicking");
+  setTimeout(function () {
+    chooseRock.classList.remove("clicking");
+  }, 50);
   playerSelection = rock;
   if (compScore < 5 && playerScore < 5) {
     game();
@@ -105,6 +119,10 @@ chooseRock.addEventListener("click", () => {
 
 const choosePaper = document.querySelector(".paper");
 choosePaper.addEventListener("click", () => {
+  choosePaper.classList.add("clicking");
+  setTimeout(function () {
+    choosePaper.classList.remove("clicking");
+  }, 50);
   playerSelection = paper;
   if (compScore < 5 && playerScore < 5) {
     game();
@@ -113,6 +131,10 @@ choosePaper.addEventListener("click", () => {
 
 const chooseScissor = document.querySelector(".scissor");
 chooseScissor.addEventListener("click", () => {
+  chooseScissor.classList.add("clicking");
+  setTimeout(function () {
+    chooseScissor.classList.remove("clicking");
+  }, 50);
   playerSelection = scissor;
   if (compScore < 5 && playerScore < 5) {
     game();
